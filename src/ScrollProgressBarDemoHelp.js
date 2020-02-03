3bee66dd674c995247578f73890161172f5a98e8
ScrollProgressBar <ScrollProgressBar@gmail.com>
2020-02-03 13:17:33 +0100

    Fixed not removing old bar

--- src/ScrollProgressBarDemoHelp.js
+++ src/ScrollProgressBarDemoHelp.js
@@ -1,41 +1,53 @@
-ScrollProgressBar.prototype.setColor = function(color) {
+ScrollProgressBar.prototype.setColor = function (color) {
   this.options.color = color;
 
   var styleS =
     document.querySelector("style") ||
     document.getElementsByTagName("style")[0];
 
+  var bar_current = document.querySelector('[id|="_scrollbar"]') || document.getElementsByName('span')[document.getElementsByName('span').length - 1];
+
+  bar_current.parentNode.removeChild(bar_current)
   styleS.parentNode.removeChild(styleS);
 
   this.start();
 };
-ScrollProgressBar.prototype.setOpacity = function(opacity) {
+ScrollProgressBar.prototype.setOpacity = function (opacity) {
   this.options.opacity = opacity;
 
   var styleS =
     document.querySelector("style") ||
     document.getElementsByTagName("style")[0];
 
+  var bar_current = document.querySelector('[id|="_scrollbar"]') || document.getElementsByName('span')[document.getElementsByName('span').length - 1];
+  bar_current.parentNode.removeChild(bar_current)
+
   styleS.parentNode.removeChild(styleS);
 
   this.start();
 };
-ScrollProgressBar.prototype.setPlacement = function(placement) {
+ScrollProgressBar.prototype.setPlacement = function (placement) {
   this.options.placement = placement;
   var styleS =
     document.querySelector("style") ||
     document.getElementsByTagName("style")[0];
 
+  var bar_current = document.querySelector('[id|="_scrollbar"]') || document.getElementsByName('span')[document.getElementsByName('span').length - 1];
+
+  bar_current.parentNode.removeChild(bar_current)
   styleS.parentNode.removeChild(styleS);
 
   this.start();
 };
-ScrollProgressBar.prototype.setSize = function(size) {
+ScrollProgressBar.prototype.setSize = function (size) {
   this.options.size = size;
   var styleS =
     document.querySelector("style") ||
     document.getElementsByTagName("style")[0];
 
+  var bar_current = document.querySelector('[id|="_scrollbar"]') || document.getElementsByName('span')[document.getElementsByName('span').length - 1];
+
+  bar_current.parentNode.removeChild(bar_current)
   styleS.parentNode.removeChild(styleS);
 
   this.start();
